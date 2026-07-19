3635e21f0e5cf73401e83e84ca981f59a85e7074
BioCidee <rudy.courtois@hotmail.com>
2026-07-19 12:59:18 +0200

    Add Player Input
    - Input Action ( Movement + Mouse Positon )
    - Rigidbody 2D

--- Assets/MyGame/Scripts/Player/Player_Movement.cs
+++ Assets/MyGame/Scripts/Player/Player_Movement.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
     [Header("---- Movement Parameters ----")]
     [SerializeField] private float movementSpeed;
+    [SerializeField] private Rigidbody2D rb;
 
+    [Header("---- Input Action Reference ----")]
+    [SerializeField] private InputActionReference inputMove;
+    [SerializeField] private InputActionReference inputMousePosition;
     private Vector2 movementDirection;
+    private Vector2 mousePosition;
+    
 
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update() {
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementDirection = inputMove.action.ReadValue<Vector2>();
+        mousePosition = inputMousePosition.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate() {
+        rb.linearVelocity = new Vector2(movementDirection.x * movementSpeed, movementDirection.y * movementSpeed);
+
+        Debug.Log(mousePosition);
+
     }
 }
