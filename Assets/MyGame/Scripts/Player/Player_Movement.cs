bdac2cc5ce21ac0fc1a3f3003d5b2bbf655019bc
BioCidee <rudy.courtois@hotmail.com>
2026-07-19 13:25:28 +0200

    Aim at mouse
    - Aim at mouse
    - Player Rotation

--- Assets/MyGame/Scripts/Player/Player_Movement.cs
+++ Assets/MyGame/Scripts/Player/Player_Movement.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player_Movement : MonoBehaviour
 {
     [Header("---- Movement Parameters ----")]
     [SerializeField] private float movementSpeed;
     [SerializeField] private Rigidbody2D rb;
+
+    [Header("---- Player Camera ----")]
+    [SerializeField] private Camera cam;
 
     [Header("---- Input Action Reference ----")]
     [SerializeField] private InputActionReference inputMove;
@@ -20,13 +24,22 @@ public class Player_Movement : MonoBehaviour
 
     private void Update() {
         movementDirection = inputMove.action.ReadValue<Vector2>();
-        mousePosition = inputMousePosition.action.ReadValue<Vector2>();
+        mousePosition = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
     }
 
     private void FixedUpdate() {
-        rb.linearVelocity = new Vector2(movementDirection.x * movementSpeed, movementDirection.y * movementSpeed);
+        PlayerRotation();
+        PlayerMovement();
+    }
 
-        Debug.Log(mousePosition);
+    private void PlayerRotation() {
+        Vector2 lookDir = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
 
+    private void PlayerMovement() {
+        rb.linearVelocity = new Vector2(movementDirection.x * movementSpeed, movementDirection.y * movementSpeed);  
     }
 }
